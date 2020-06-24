118e551752cae0eb04daa38c5e6e01879f766057
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2020-06-25 00:53:36 +0300

    Compatibiliy with Node 14

--- src/function_origin.cc
+++ src/function_origin.cc
@@ -3,28 +3,26 @@
 
 using namespace v8;
 
-NAN_METHOD(SetOrigin)
-{
+NAN_METHOD(SetOrigin) {
   Local<Function> fn = info[0].As<Function>();
   Local<Object> target = info[1].As<Object>();
   ScriptOrigin origin = fn->GetScriptOrigin();
 
-  target->Set(Nan::New<String>("file").ToLocalChecked(), origin.ResourceName());
+  Nan::Set(target, Nan::New<String>("file").ToLocalChecked(), origin.ResourceName()).Check();
 
-  target->Set(Nan::New<String>("line").ToLocalChecked(),
-              Nan::New<Integer>(fn->GetScriptLineNumber()));
+  Nan::Set(target, Nan::New<String>("line").ToLocalChecked(),
+              Nan::New<Integer>(fn->GetScriptLineNumber())).Check();
 
-  target->Set(Nan::New<String>("column").ToLocalChecked(),
-              Nan::New<Integer>(fn->GetScriptColumnNumber()));
+  Nan::Set(target, Nan::New<String>("column").ToLocalChecked(),
+              Nan::New<Integer>(fn->GetScriptColumnNumber())).Check();;
 
-  target->Set(Nan::New<String>("inferredName").ToLocalChecked(), fn->GetInferredName());
+  Nan::Set(target, Nan::New<String>("inferredName").ToLocalChecked(), fn->GetInferredName()).Check();
 }
 
-static void Init(Local<v8::Object> exports)
-{
+static void Init(Local<v8::Object> exports) {
   v8::Local<v8::Context> context = exports->CreationContext();
-  exports->Set(Nan::New<String>("SetOrigin").ToLocalChecked(),
-               Nan::New<FunctionTemplate>(SetOrigin)->GetFunction(context).ToLocalChecked());
+  Nan::Set(exports, Nan::New<String>("SetOrigin").ToLocalChecked(),
+               Nan::New<FunctionTemplate>(SetOrigin)->GetFunction(context).ToLocalChecked()).Check();
 }
 
 NODE_MODULE(function_origin, Init)
